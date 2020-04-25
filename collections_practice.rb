0f457a2d929fd0a4d5f234c935edef3247d65c91
Joey <joey.alzamora@gmail.com>
2020-04-25 01:12:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,10 +38,23 @@ def find_a(array)
  end
 end
 
-def sum_array
-end
+def sum_array(array)
+  sum = 0 
+  array.each do |num|
+    sum += num
+  end
+   sum
+ end
 
-def add_s
+def add_s(array)
+  array.collect do |word|
+    if array [1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
 end
 
 
